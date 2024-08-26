c7c15f04e3f1d12f052848f8f8977ea40c2f3f0e
marksda <vlack123@gmail.com>
2024-08-26 19:53:10 +0700

    cso

--- templates/project/simple/loader.php
+++ templates/project/simple/loader.php
@@ -11,14 +11,3 @@ $loader->setDirectories(
 );
 
 $loader->register();
-
-
-/**
- * We're a registering a set of directories taken from the configuration file
- */
-// $loader->registerDirs(
-//     [
-//         $config->application->controllersDir,
-//         $config->application->modelsDir
-//     ]
-// )->register();
